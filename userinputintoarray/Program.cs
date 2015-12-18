718b3602b9c852cae795a6cea9487f6d0e5ecf30
Ikram_Lim <ikramlim@gmail.com>
2015-12-18 18:44:03 +0800

    user input
    user input into array

---
+++ userinputintoarray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userinputintoarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many integers will you enter? \n");
+
+            string amount = Console.ReadLine();
+            int arrayAmount = Convert.ToInt32(amount);
+            int[] array = new int[arrayAmount];
+
+            for(int i=0; i<array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The values you've entered are");
+            Console.WriteLine("{0}{1,8}", "index", "value");
+            for (int counter = 0; counter < array.Length; counter++)
+                Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
+            Console.ReadLine();
+        }
+    }
+}
